5d38726011b3e94d5b9f5f2ae941c32d3ca0abf2
Qudi Documentation Builder <qudi@uni-ulm.de>
2017-01-30 15:37:42 +0000

    Automated documentation build for changeset ebc5542cfbcc9953cdf2592f2eb9fbaa7e724cd4.

---
+++ html-docs/search/pages_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['interfuses',['Interfuses',['../how_to_interfuses.html',1,'']]],
+  ['installation',['Installation',['../installation.html',1,'']]]
+];
---
+++ html-docs/search/variables_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['x_5fshift_5fplot',['x_shift_plot',['../classgui_1_1poimanager_1_1poimangui_1_1PoiManagerGui.html#a5ae357d05af2e5f182ed7c291124032f',1,'gui::poimanager::poimangui::PoiManagerGui']]],
+  ['xy_5fchannel',['xy_channel',['../classgui_1_1confocal_1_1confocalgui_1_1ConfocalGui.html#ad65f259d130aee84565dcaf01cd4a114',1,'gui::confocal::confocalgui::ConfocalGui']]],
+  ['xy_5frefocus_5fimage',['xy_refocus_image',['../classgui_1_1confocal_1_1confocalgui_1_1ConfocalGui.html#a1c5c40d0818c371f0c4e20c29234da5a',1,'gui::confocal::confocalgui::ConfocalGui']]]
+];
