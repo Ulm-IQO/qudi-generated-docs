8507112f53765a6934f999abc2337a3031939072
Qudi Documentation Builder <qudi@uni-ulm.de>
2017-03-18 00:04:19 +0000

    Automated documentation build for changeset 50b5ad9acb400f9d1c1a408260c4ae2e0104816b.

--- html-docs/search/pages_1.js
+++ html-docs/search/pages_1.js
@@ -5,7 +5,6 @@ var searchData=
   ['how_20to_20use_20fitting_20in_20qudi',['How to use fitting in Qudi',['../fit_logic.html',1,'']]],
   ['how_20to_20use_20the_20qudi_20jupyter_20notebook',['How to use the Qudi Jupyter Notebook',['../jupyterkernel.html',1,'']]],
   ['how_20to_20use_20the_20logger_20in_20qudi',['How to use the logger in Qudi',['../logger_usage.html',1,'']]],
-  ['how_20to_20run_20a_20pulsed_20measurement',['How to run a pulsed measurement',['../pulsedmeasurement-explanation.html',1,'']]],
   ['how_20to_20use_20and_20create_20tasks',['How to use and create tasks',['../task-usage.html',1,'']]],
   ['how_20to_20write_20easily_20documentations_20in_20markdown_20notation',['How to write easily documentations in markdown notation',['../write-markdown.html',1,'']]]
 ];
