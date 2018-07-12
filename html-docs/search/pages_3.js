c8e4e6ef32681934ed5a0e1d4263277e16cf899b
Qudi Documentation Builder <qudi@uni-ulm.de>
2018-07-12 13:11:51 +0000

    Automated documentation build for changeset ad222b3e209b987cf2ebd65f49200c79dd17ead6.

--- html-docs/search/pages_3.js
+++ html-docs/search/pages_3.js
@@ -7,6 +7,7 @@ var searchData=
   ['how_20to_20use_20the_20qudi_20jupyter_20notebook',['How to use the Qudi Jupyter Notebook',['../jupyterkernel.html',1,'']]],
   ['how_20to_20use_20the_20logger_20in_20qudi',['How to use the logger in Qudi',['../logger_usage.html',1,'']]],
   ['how_20to_20create_20modules',['How to create modules',['../make-modules.html',1,'']]],
+  ['how_20to_20participate',['How to participate',['../participate.html',1,'']]],
   ['how_20to_20use_20and_20create_20tasks',['How to use and create tasks',['../task-usage.html',1,'']]],
   ['how_20to_20write_20easily_20documentations_20in_20markdown_20notation',['How to write easily documentations in markdown notation',['../write-markdown.html',1,'']]]
 ];
